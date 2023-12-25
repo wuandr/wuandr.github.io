47c2dd6ac5f8410ac248701e89a2665d14fdfbbf
wuandr <wuandrewj@gmail.com>
2023-12-24 20:12:51 -0500

    messing around, nothing working yet

---
+++ index.ts
@@ -0,0 +1,5 @@
+window.document.getElementById("testBtn")?.addEventListener("click", logClick);
+
+function logClick() {
+    console.log("btn was clicked");
+}
---
+++ server.ts
@@ -0,0 +1,11 @@
+function run(): void {
+    const port = process.env['PORT'] || 4000;
+
+    // Start up the Node server
+    const server = app();
+    server.listen(port, () => {
+        console.log(`Node Express server listening on http://localhost:${port}`);
+    });
+}
+
+run();
